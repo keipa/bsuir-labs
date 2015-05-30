d57c2b439a007f29ea95ed99accd43599eb160f8
nickolai rovdo <nichogaus@gmail.com>
2015-05-31 01:06:48 +0300

    LAB 8 ready
    read more about "lambda"
    interact more with copcod<arm>

---
+++ 2 cem/C SHARP/lab_8/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace op
+{
+
+    class Problems
+    {
+        static public uint BecauseICan(int a)
+        {
+            Console.WriteLine("It is working!!!");
+            return 0;
+        }
+
+        static public uint EqualsNumer(int a)
+        {
+            Console.WriteLine("Beware! Numbers are equals");
+            return 1;
+        }
+
+        static public uint LessZero(int a)
+        {
+            Console.WriteLine("Alert!!! Number is less the zero");
+            return (uint)Math.Abs(a);
+        }
+
+        static public uint ZeroEquals(int a)
+        {
+            Console.WriteLine("Alert!!! Number is equal to zero");
+            return 1;
+        }
+    }
+    delegate uint ProblemMethod(int a);
+
+    class Rational : IEquatable<Rational>, IComparable<Rational>
+    {
+        private uint n;
+        private uint m;
+
+        public Rational() { }
+
+        public Rational(uint a, uint b)
+        {
+            n = a;
+            m = (b > 0) ? b : 1;
+        }
+
+        public event ProblemMethod ZeroCheck;
+        public event ProblemMethod ZeroCheck1;
+
+        public uint N
+        {
+            get { return n; }
+            set
+            {
+                if (value == 0)
+                {
+                    if (ZeroCheck != null)
+                    {
+                        n = ZeroCheck((int)value);
+                    }
+                    else
+                    {
+                        n = 1;
+                    }
+                }
+                else
+                {
+                    n = value;
+                }
+            }
+
+        }
+
+        public uint M
+        {
+            get { return m; }
+            set
+            {
+                if (value == 0)
+                {
+                    if (ZeroCheck1 != null)
+                    {
+                        m = ZeroCheck1((int)value);
+                    }
+                    else
+                    {
+                        m = 1;
+                    }
+                }
+                else
+                {
+                    m = value;
+                }
+            }
+        }
+
+        public static Rational operator +(Rational a, Rational b)
+        {
+            Rational result = new Rational(1, 1);
+
+            result.m = a.m * b.m;
+            result.n = result.m / a.n + result.m / b.n;
+
+            return result;
+        }
+
+        public static Rational operator -(Rational a, Rational b)
+        {
+            Rational result = new Rational(1, 1);
+
+            result.m = a.m * b.m;
+            result.n = result.m / a.n - result.m / b.n;
+
+            return result;
+        }
+
+        public static Rational operator *(Rational a, Rational b)
+        {
+            Rational result = new Rational();
+            result.n = a.n * b.n;
+            result.m = a.m * b.m;
+            return result;
+        }
+
+        public static Rational operator /(Rational a, Rational b)
+        {
+            Rational result = new Rational();
+            result.n = a.n * b.m;
+            result.m = a.m * b.n;
+            return result;
+        }
+
+        public static bool operator >(Rational a, Rational b)
+        {
+            if (a.CompareTo(b) == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >=(Rational a, Rational b)
+        {
+            if (a.CompareTo(b) >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Rational a, Rational b)
+        {
+            if (a.CompareTo(b) == -1)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <=(Rational a, Rational b)
+        {
+            if (a.CompareTo(b) <= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Rational a, Rational b)
+        {
+            if (a.n == b.n && a.m == b.m)
+                return false;
+            else
+                return true;
+        }
+
+        public static bool operator ==(Rational a, Rational b)
+        {
+            return a.Equals(b);
+        }
+
+        public static implicit operator Rational(uint a)
+        {
+            return new Rational(a, 1);
+        }
+
+        public static explicit operator double(Rational a)
+        {
+            return (a.N / a.M);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", n, m);
+        }
+
+        public string ToStringDecFrac(int r)
+        {
+            double DecFrac = n / m;
+            return string.Format("{0}", DecFrac);
+        }
+
+        public bool Equals(Rational other)
+        {
+            if (this.n == other.n && this.m == other.m)
+                return true;
+            else
+                return false;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            Rational RationalObj = obj as Rational;
+            return Equals(RationalObj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (this.n / this.m).GetHashCode();
+        }
+
+        public int CompareTo(Rational other)
+        {
+            if (this.Equals(other)) return 0;
+            else if (this.m > other.m) return 1;
+            else if (this.m < other.m) return -1;
+            else if (this.m == other.m && this.n > other.n) return 1;
+            else if (this.m == other.m && this.n < other.n) return -1;
+            else return 1;
+        }
+    }
+
+    static class op
+    {
+        static public void Main()
+        {
+            Rational first = new Rational();
+            first.ZeroCheck += Problems.BecauseICan;
+            first.ZeroCheck += Problems.ZeroEquals;
+
+            Console.WriteLine("Read rational number as a fraction");
+            string ast = Console.ReadLine();
+            string bst = Console.ReadLine();
+
+            ProblemMethod first_problem = new ProblemMethod(Problems.LessZero);
+            ProblemMethod second_problem = delegate(int a)
+            {
+                Console.WriteLine("Alert!!! Number is --------------less the zero");
+                return (uint)Math.Abs(a);
+            };
+            Func<int, uint> third_problem = Problems.LessZero;
+            ProblemMethod fourth_problem = (int a) => Problems.LessZero(a);
+
+
+            try
+            {
+                first.N = Convert.ToUInt16(ast);
+            }
+            catch (OverflowException)
+            {
+                first.N = first_problem(Convert.ToInt16(ast));
+            }
+
+            try
+            {
+                first.M = Convert.ToUInt16(bst);
+            }
+            catch (OverflowException)
+            {
+                first.M = second_problem(Convert.ToInt16(bst));
+            }
+
+            Console.WriteLine("Nothing");
+            Console.WriteLine(first.ToString());
+            Console.WriteLine(first.ToStringDecFrac(0));
+
+            Rational second = new Rational();
+            Console.WriteLine("Read rational number as string");
+            string rational_number = Console.ReadLine();
+            Regex slash = new Regex("/");
+            string[] two_numbers = slash.Split(rational_number);
+            first_problem = new ProblemMethod(Problems.LessZero);
+
+            try
+            {
+                second.N = Convert.ToUInt16(two_numbers[0]);
+            }
+            catch (OverflowException)
+            {
+                second.N = third_problem(Convert.ToInt16(two_numbers[0]));
+            }
+
+            try
+            {
+                second.M = Convert.ToUInt16(two_numbers[1]);
+            }
+            catch (OverflowException)
+            {
+                second.M = fourth_problem(Convert.ToInt16(two_numbers[1]));
+            }
+            Console.WriteLine(second.ToString());
+            Console.WriteLine(second.ToStringDecFrac(0));
+
+            Console.WriteLine("Does First equals to Second?");
+            if ((object)first == (object)second)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.WriteLine("What is more?");
+            Console.WriteLine(first.CompareTo(second));
+
+            first = 84927;
+            Console.WriteLine(first.ToStringDecFrac(0));
+
+            double end = (double)second;
+            Console.WriteLine(end);
+        }
+    }
+}
---
+++ 2 cem/C SHARP/lab_8/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace Program
+{
+    class Exepts
+    {
+        static public int trythis(int a)
+        {
+            Console.WriteLine("Work trying");
+            return 1;
+        }
+
+        static public int equalsnumber(int a)
+        {
+            Console.WriteLine("del equnum");
+            return 1;
+        }
+
+        static public int subzero(int a)
+        {
+            Console.WriteLine("under the zero");
+            return Math.Abs(a);
+        }
+
+        static public int zeroeq(int a)
+        {
+            Console.WriteLine("number is equal zero");
+            return 1;
+        }
+    }
+
+    delegate int MyFirstdel(int a);
+    class Ratio : IEquatable<Ratio>, IComparable<Ratio>
+    {
+        private int upnum;
+        private int downnum;
+
+        public Ratio(int a, int b)
+        {
+            upnum = a;
+            downnum = b;
+        }
+
+        public Ratio()   //конструктор без параметров
+        {
+            upnum = 0;
+            downnum = 1; //do not equal 0
+        }
+        public event MyFirstdel CheckNull;
+
+        public int Up   //поле числителя
+        {
+            get
+            {
+                return upnum;
+            }
+            set
+            {
+                upnum = value;
+            }
+        }
+
+        public int Down // поле знаменателя
+        {
+            get
+            {
+                return downnum;
+            }
+            set
+            {
+                if (value != 0)
+                    downnum = value;
+            }
+        }
+
+        /*
+         перегразка операторов
+         * операторов
+         
+         
+         */
+
+        public static Ratio operator +(Ratio c1, Ratio c2)
+        {
+            Ratio temp = new Ratio();
+            temp.upnum = c1.upnum * c2.downnum + c2.upnum * c1.downnum;
+            temp.downnum = c1.downnum * c2.downnum;
+            return temp;
+        }
+
+        public static Ratio operator -(Ratio c1, Ratio c2)
+        {
+            Ratio temp = new Ratio();
+            temp.upnum = c1.upnum * c2.downnum - c2.upnum * c1.downnum;
+            temp.downnum = c1.downnum * c2.downnum;
+            return temp;
+        }
+
+        public static Ratio operator *(Ratio c1, Ratio c2)
+        {
+            Ratio temp = new Ratio();
+            temp.upnum = c1.upnum * c2.upnum;
+            temp.downnum = c1.downnum * c2.downnum;
+            return temp;
+        }
+
+        public static Ratio operator /(Ratio c1, Ratio c2)
+        {
+            Ratio temp = new Ratio();
+            temp.upnum = c1.upnum * c2.downnum;
+            temp.downnum = c1.downnum * c2.upnum;
+            return temp;
+        }
+
+        public static bool operator ==(Ratio c1, Ratio c2)  //оператор == заменён
+        {
+            if (c1.Equals(c2))
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Ratio c1, Ratio c2)
+        {
+            if (c1.Equals(c2))
+                return false;
+            else
+                return true;
+        }
+
+        public static bool operator >(Ratio c1, Ratio c2)
+        {
+            if (c1.CompareTo(c2) == 1)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator >=(Ratio c1, Ratio c2)
+        {
+            if (c1.CompareTo(c2) >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Ratio c1, Ratio c2)
+        {
+            if (c1.CompareTo(c2) == -1)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <=(Ratio c1, Ratio c2)
+        {
+            if (c1.CompareTo(c2) <= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public string toString1()       //первый метод вывода
+        {
+            return String.Format("{0}/{1}", Up, Down);
+        }
+
+        public string toString2() //второй метод вывода
+        {
+            return String.Format("Up:{0}\nDown:{1}", Up, Down);
+        }
+
+        public static Ratio Parse(string input)  //парсинг строки и преобразование её в дробь
+        {
+            Regex regex = new Regex("\\d+");   //регулярное выражение 
+            Match match = regex.Match(input);  //совпадают только отдельные числа
+            Ratio temp = new Ratio();
+            temp.Up = Int32.Parse(match.Groups[0].Value);
+            match = match.NextMatch();
+            temp.Down = Int32.Parse(match.Groups[0].Value);
+            return temp;
+        }
+
+        public bool Equals(Ratio temp)//метод equal требудет присутствия gethashcode для безопасной работы с экземплярам
+        {
+            if (temp.Up * temp.Down == this.Up * this.Down)
+                return true;
+            else
+                return false;
+        }
+
+        public override bool Equals(Object obj)   //сравнение с приведением типов
+        {
+            Ratio RatioObj = obj as Ratio;
+            return Equals(RatioObj);
+        }
+
+        public override int GetHashCode()     //шанс коллизий
+        {
+            return this.upnum.GetHashCode();
+        }
+
+        public int CompareTo(Ratio temp)   //сравнение 
+        {
+            if (this.Equals(temp))
+                return 0;
+            else if (temp.Up / temp.Down < this.Up * this.Down)
+                return -1;
+            else
+                return 1;
+        }
+
+        public static explicit operator int(Ratio temp)//явное приведение
+        {
+            int rezult;
+            if (temp.Up % temp.Down == 0)
+                rezult = temp.Up / temp.Down;
+            else
+                rezult = -1;
+            return rezult;
+        }
+
+        public static implicit operator double(Ratio temp)  //неявное приведение
+        {
+            return (double)temp.Up / temp.Down;
+        }
+    }
+
+    class Program
+    {
+        static int Main()
+        {
+            Ratio r1 = new Ratio(4, 0);
+
+            MyFirstdel one = new MyFirstdel(Exepts.trythis);
+            MyFirstdel two = delegate(int a) {
+                Console.WriteLine("Secnd trying");
+                return a;
+            };
+            
+
+            Func<int, int> tre = Exepts.trythis;
+
+
+            MyFirstdel four = (int a) => Exepts.trythis(a);
+            try
+            {
+                int e = r1.Up / r1.Down;
+            }
+            catch (DivideByZeroException)
+            {
+                r1.Down = one(0);
+                Console.WriteLine(r1.Down);
+            }
+            // int intr = (int)r1;
+            //double doubler = r1;
+            Ratio r2 = new Ratio(3, 4);
+            Ratio r3 = r1 * r2;
+            r3.Down = 0;
+            r3 = Ratio.Parse("Up: 12, Down = 3");
+            Console.WriteLine("{0}\n--\n{1}\n", r3.Up, r3.Down);
+            Console.WriteLine(r3.toString1());
+            Console.WriteLine(r3.toString2());
+            //  Console.WriteLine("{0} {1}", intr, doubler);
+            if (r1 == r2)
+            {
+                Console.WriteLine("equal");
+            }
+            else
+                Console.WriteLine("not equal");
+            Console.ReadKey();
+            return 0;
+
+        }
+    }
+}
