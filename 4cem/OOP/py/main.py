bf50ff183f80eecc5320c5da302cadb704062be7
nickolai rovdo <nichogaus@gmail.com>
2016-03-16 00:48:38 +0300

    4.oop falling and moving added

--- 4cem/OOP/py/main.py
+++ 4cem/OOP/py/main.py
@@ -1,82 +1,121 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Импортируем библиотеку pygame
 import pygame
 import miner
 from pygame import *
 
-#Объявляем переменные
-window_width = 800 #Ширина создаваемого окна
-window_height = 600 # Высота
-display_dict = (window_width, window_height) # Группируем ширину и высоту в одну переменную
+window_width = 800
+window_height = 640
+display_dict = (window_width, window_height)
 background_color = "#003300"
 
+
+class Camera(object):
+    def __init__(self, camera_func, width, height):
+        self.camera_func = camera_func
+        self.state = Rect(0, 0, width, height)
+
+    def apply(self, target):
+        return target.rect.move(self.state.topleft)
+
+    def update(self, target):
+        self.state = self.camera_func(self.state, target.rect)
+
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l+window_width / 2, -t+window_height / 2
+
+    l = min(0, l)
+    l = max(-(camera.width-window_width), l)
+    t = max(-(camera.height-window_height), t)
+    t = min(0, t)
+    return Rect(l, t, w, h)
+
+
 def main():
-    pygame.init() # Инициация PyGame, обязательная строчка
-    screen = pygame.display.set_mode(display_dict) # Создаем окошко
-    pygame.display.set_caption("Space Miner") # Пишем в шапку
-    bg = Surface(display_dict) # Создание видимой поверхности
+    pygame.init()  # Инициация PyGame, обязательная строчка
+    screen = pygame.display.set_mode(display_dict)  # Создаем окошко
+    pygame.display.set_caption("Space Miner")  # Пишем в шапку
+    timer = pygame.time.Clock()
+    bg = Surface(display_dict)  # Создание видимой поверхности
     bg.fill(Color(background_color))
-    hero = miner.Miner(55, 55)
+    hero = miner.Miner(55, 155)
     left = right = False
+    up = False
     level = [
-           "-------------------------",
-           "-                       -",
-           "-                       -",
-           "-                       -",
-           "-            --         -",
-           "-                       -",
-           "--                      -",
-           "-                       -",
-           "-                   --- -",
-           "-                       -",
-           "-                       -",
-           "-      ---              -",
-           "-                       -",
-           "-   -----------         -",
-           "-                       -",
-           "-                -      -",
-           "-                   --  -",
-           "-                       -",
-           "-                       -",
-           "-***********************-"]
+       "-------------------------",
+       "-                       -",
+       "-                       -",
+       "-                       -",
+       "-            --         -",
+       "-                       -",
+       "--                      -",
+       "-                       -",
+       "-                   --- -",
+       "-                       -",
+       "-                       -",
+       "-      ---              -",
+       "-                       -",
+       "-   -----------         -",
+       "-                       -",
+       "-                -      -",
+       "-                   --  -",
+       "-                       -",
+       "-                       -",
+       "-------------------------"]
     platform_width = 32
     platform_height = 32
+    platform = (platform_width, platform_height)
     platform_color = "#006262"
     ground_color = "#124315"
-    x = y = 0
-    for row in level:
-        for col in row:
-            if col == "-":
-                bg = Surface(display_dict)
-                bg.fill(Color(platform_color))
-                screen.blit(bg, (x, y))
-            if col == "*":
-                bg = Surface(display_dict)
-                bg.fill(Color(ground_color))
-                screen.blit(bg, (x, y))
-            x += platform_width
-        y += platform_height
-        x = 0
 
+    # total_level_width = len(level[0])*platform_width # Высчитываем фактическую ширину уровня
+    # total_level_height = len(level)*platform_height   # высоту
+    #
+    # camera = Camera(camera_configure, total_level_width, total_level_height)
     while 1:
+        timer.tick(80)
+        x = y = 0
+        for row in level:
+            for col in row:
+                if col == "-":
+                    bg = Surface(platform)
+                    bg.fill(Color(platform_color))
+                    screen.blit(bg, (x, y))
+                else:
+                    bg = Surface(platform)
+                    bg.fill(Color(ground_color))
+                    screen.blit(bg, (x, y))
+                x += platform_width
+            y += platform_height
+            x = 0
         for e in pygame.event.get():
             if e.type == QUIT:
-                raise SystemExit, "QUIT"
+                raise (SystemExit, "QUIT")
+            if e.type == KEYDOWN and e.key == K_UP:
+                up = True
+            if e.type == KEYUP and e.key == K_UP:
+                up = False
             if e.type == KEYDOWN and e.key == K_LEFT:
+                print "hero turn left"
                 left = True
             if e.type == KEYDOWN and e.key == K_RIGHT:
+                print "hero turn right"
                 right = True
             if e.type == KEYUP and e.key == K_RIGHT:
                 right = False
             if e.type == KEYUP and e.key == K_LEFT:
                 left = False
 
-        screen.blit(bg, (0, 0))      # Каждую итерацию необходимо всё перерисовывать
-        hero.update(left, right)
+        # camera.update(hero)  # центризируем камеру относительно персонажа
+
+        hero.update(left, right, up)
         hero.draw(screen)
-        pygame.display.update()     # обновление и вывод всех изменений на экран
+        screen.blit(bg, (0, 0))
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
--- 4cem/OOP/py/miner.py
+++ 4cem/OOP/py/miner.py
@@ -7,11 +7,15 @@ move_speed = 7
 width = 22
 height = 32
 miner_color = "#888888"
+JUMP_POWER = 10
+GRAVITY = 0.35
 
 
 class Miner(sprite.Sprite):
     def __init__(self, x, y):
         sprite.Sprite.__init__(self)
+        self.vertical_speed = 0 # скорость вертикального перемещения
+        self.onGround = False # На земле ли я?
         self.current_speed = 0
         self.start_x = x
         self.start_y = y
@@ -19,16 +23,20 @@ class Miner(sprite.Sprite):
         self.image.fill(Color(miner_color))
         self.rect = Rect(x, y, width, height)
 
-    def update(self,  left, right):
+    def update(self,  left, right, up):
+        if up:
+            if self.onGround:  # прыгаем, только когда можем оттолкнуться от земли
+                self.vertical_speed = -JUMP_POWER
+            if not self.onGround:
+                self.vertical_speed += GRAVITY
+            self.onGround = False  # Мы не знаем, когда мы на земле((
+            self.rect.y += self.vertical_speed
         if left:
             self.current_speed = -move_speed
-
         if right:
             self.current_speed = move_speed
-
         if not(left or right):
             self.current_speed = 0
-
         self.rect.x += self.current_speed
 
     def draw(self, screen):
