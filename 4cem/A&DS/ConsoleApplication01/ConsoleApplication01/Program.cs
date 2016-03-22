af2727eb1780ae1085315ea9a861226b08b0ba56
nickolai rovdo <nichogaus@gmail.com>
2016-03-22 10:34:20 +0300

    4.A&DS 03 254 symbols

--- 4cem/A&DS/ConsoleApplication01/ConsoleApplication01/Program.cs
+++ 4cem/A&DS/ConsoleApplication01/ConsoleApplication01/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections;
 
 namespace ConsoleApplication01
 {
@@ -10,6 +11,7 @@ namespace ConsoleApplication01
         static void Main(string[] args)
         {
             int a = Convert.ToInt32(Console.ReadLine());
+            
             int[,] exclude = new int[a, a];
             for (int i = 0; i < a; i++)
             {
@@ -18,88 +20,40 @@ namespace ConsoleApplication01
                     exclude[i, j] = 0;
                 }
             }
-            List<int> left_graph = new List<int>();
-            List<int> right_graph = new List<int>();
             for (int i = 1; i < a; i++)
             {
                 string[] t = Console.ReadLine().Split(' ');
-                
-                
                 exclude[Convert.ToInt32(t[0]) - 1, Convert.ToInt32(t[1]) - 1] = 1;
                 exclude[Convert.ToInt32(t[1]) - 1, Convert.ToInt32(t[0]) - 1] = -1;
-
-                //left_graph.Add(Convert.ToInt32(t[0]));
-                //right_graph.Add(Convert.ToInt32(t[1]));
-
             }
-            Console.WriteLine(rank(exclude, a)/2);
+            Console.WriteLine(rank(exclude, a) / 2);
         }
 
         private static int rank(int[,] exclude, int a)
         {
             const double EPS = 1E-9;
-
             int rank = a;
             bool[] line_used = new bool[a];
-            for (int i=0; i<a; ++i) {
-	            int j;
-	            for (j=0; j<a; ++j)
-		            if (!line_used[j] && Math.Abs(exclude[j,i]) > EPS)
-			            break;
-	            if (j == a)
-		            --rank;
-	            else {
-		            line_used[j] = true;
-		            for (int p=i+1; p<a; ++p)
-			            exclude[j,p] /= exclude[j,i];
-		            for (int k=0; k<a; ++k)
-			            if (k != j && Math.Abs(exclude[k,i]) > EPS)
-				            for (int p=i+1; p<a; ++p)
-					            exclude[k,p] -= exclude[j,p] * exclude[k,i];
-	            }
+            for (int i = 0; i < a; ++i)
+            {
+                int j;
+                for (j = 0; j < a; ++j)
+                    if (!line_used[j] && Math.Abs(exclude[j, i]) > EPS)
+                        break;
+                if (j == a)
+                    --rank;
+                else
+                {
+                    line_used[j] = true;
+                    for (int p = i + 1; p < a; ++p)
+                        exclude[j, p] /= exclude[j, i];
+                    for (int k = 0; k < a; ++k)
+                        if (k != j && Math.Abs(exclude[k, i]) > EPS)
+                            for (int p = i + 1; p < a; ++p)
+                                exclude[k, p] -= exclude[j, p] * exclude[k, i];
+                }
             }
-
             return rank;
         }
-
-
-
-
-
-
-
-        //public static int rank(int[,] matrix, int a)
-        //{
-        //    int rank = a;
-        //    for (int j = 0; j < a; j++)
-        //    {
-        //        for (int i = 0; i < a; i++)
-        //        {
-        //            if (i > j)
-        //            {
-        //                int c = 0;
-        //                if (matrix[j, i] == 0)
-        //                {
-        //                    --rank;
-        //                }
-        //                else
-        //                {
-        //                    c = matrix[i, j] / matrix[j, i];
-        //                }
-        //                for (int k = 0; k <= a; k++)
-        //                {
-        //                    matrix[i, k] -= c * matrix[j, k];
-        //                }
-        //            }
-        //        }
-        //    }
-        //    for (int i = 0; i < a; i++)
-        //    {
-        //        for (int j = 0; j < a; j++)
-        //            Console.Write(matrix[i, j] + "\t");
-        //        Console.WriteLine();
-        //    }
-        //    return 0;
-        //}
     }
 }
---
+++ 4cem/A&DS/ConsoleApplication03/ConsoleApplication03/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string str = Console.ReadLine();
+            int poly = str.Length;
+            bool brk = false;
+            List<string> sub = showSubstrings(str);
+            //Console.WriteLine(Reverse(str));
+            //Console.WriteLine(str.Substring(2).Remove(2));
+            //Console.ReadKey();
+
+            //Console.WriteLine(isPoly("kek", str));
+            for (int i = sub.Count-1; i > 0;i-- )
+            {
+                if (!isPoly(sub[i]))
+                {
+                    Console.WriteLine(sub[i].Length);
+                    brk = true;
+                    break;
+                }
+            }
+            if (!brk)
+            {
+                Console.WriteLine(-1);
+            }
+            Console.ReadKey();
+
+            //for (int p = str.Length; p > 0; p--)
+            //{
+            //    for (int i = 0; i <= str.Length - p; i++)
+            //    {
+            //        //if (p <= i)
+            //        //{
+            //        //    brk = true;
+            //        //    break;
+            //        //}
+            //        if (p == str.Length && i == 0)
+            //        {
+            //            if (!isPoly(str))
+            //            {
+            //                Console.WriteLine(poly);
+            //                brk = true;
+            //                break;
+            //            }
+            //            continue;
+            //        }
+            //        if (!isPoly(str.Substring(i).Remove(p-i)))
+            //        {
+            //            Console.WriteLine(poly);
+            //            brk = true;
+            //            break;
+            //        }
+            //    }
+            //    if (brk)
+            //        break;
+            //    --poly;
+            //    if (poly == 1)
+            //        brk = true;
+            //}
+            ////Console.WriteLine(poly);
+            //if (poly == 1)
+            //{
+            //    Console.WriteLine(-1);
+            //}
+            //Console.ReadKey();
+        }
+        public static bool isPoly(string substr)
+        {
+            if (Reverse(substr) == substr)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static List<String> showSubstrings(string str)
+        {
+            List<String> all = new List<string>();
+            int len = 1;
+            //all.Add(str);
+            while (len <= str.Length)
+            {
+                for (int i = 0; i < str.Length && i+len <= str.Length; i++)
+                {
+                    all.Add(str.Substring(i, len));
+                }
+
+                ++len;
+            }
+                return all;
+
+        }
+        public static string Reverse(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+            return reverse;
+        }
+
+    }
+}
