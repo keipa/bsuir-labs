0d2ae6ea84203872ee33c66344edcd8daf5e9c52
keipa <nichogaus@gmail.com>
2016-04-14 22:42:00 +0300

    4.py.2 generation customization

--- 4cem/PYTHON/lab2/main.py
+++ 4cem/PYTHON/lab2/main.py
@@ -1,5 +1,5 @@
-from random import *
 import argparse
+from generate import *
 
 
 def create_parser():
@@ -8,45 +8,10 @@ def create_parser():
     parser.add_argument('-n', "--nums",  default='0')
     parser.add_argument('-b', "--fixedblocks",  default='0')
     parser.add_argument('-l', "--fixedlines",  default='0')
+    parser.add_argument('-s', "--symbols", default='0')
     parser.add_argument('-ls', "--lineseparator", default='\n')
     parser.add_argument('-bs', "--blockseparator", default='\t')
     return parser
-
-
-# default line_separator - \n
-# default block_separator - \t
-# default isfixedblocks = true
-
-def generate(block_count, block_separator,  line_count,line_separator, isnums):
-
-    count_of_strings = randint(10, 100)
-    # count of strings
-    count_of_blocks = randint(1, 30)
-    # count of blocks in strings
-    count_of_letters = randint(1, 10)
-    # count of letters in blocks
-    if line_separator == "\\n":
-        line_separator = "\n"
-    if block_separator == "\\t":
-        block_separator = "\t"
-    s = ""
-    with open("strfiles/genrtd.txt", "w") as f:
-        for string in range(count_of_strings):
-            for block in range(count_of_blocks):
-                for letter in range(count_of_letters):
-                    s += random_chr(isnums)
-                if block == count_of_blocks-1:
-                    s += line_separator
-                else:
-                    s += block_separator
-            f.write(s)
-            s = ""
-
-def random_chr(isnums):
-    if isnums == '0':
-        return str(randint(0, 9))
-    else:
-        return chr(randint(97, 122))
 
 
 def menu():
@@ -57,7 +22,7 @@ def menu():
                  namespace.blockseparator,
                  int(namespace.fixedlines),
                  namespace.lineseparator,
-                 namespace.nums)
+                 namespace.nums, int(namespace.symbols))
 
 
 if __name__ == '__main__':
