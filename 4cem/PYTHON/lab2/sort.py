a51b195ea26403d0845d2c898aebbf11d375213a
keipa <nichogaus@gmail.com>
2016-04-16 15:18:35 +0300

    4.py.2 split

--- 4cem/PYTHON/lab2/sort.py
+++ 4cem/PYTHON/lab2/sort.py
@@ -2,9 +2,24 @@ from  tempfile import *
 
 
 def sort():
-    with open("strfiles/genrated.txt", "r") as file:
-        print(file.read(10))
+    split_file(10,"\n","strfiles/genrated.txt")
 
+def split_file(buffer_size, string_separator, path):
+    with open(path, "r") as file:
+        count_lines = 0
+        file_count = 1
+        for line in file:
+
+            if count_lines == 0:
+                dest = open("strfiles/tmp/generated" + str(file_count) + '.txt', 'w')
+            else:
+                dest = open("strfiles/tmp/generated" + str(file_count) + '.txt', 'a')
+            dest.write(line)
+            count_lines += 1
+
+            if count_lines > buffer_size-1:
+                file_count += 1
+                count_lines = 0
 
 if __name__ == '__main__':
     sort()
