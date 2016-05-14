247e1149689713e148161551558e3fb795ea7cc3
keipa <nichogaus@gmail.com>
2016-05-14 17:13:00 +0300

    4.py.2 logger paramerized

--- 4cem/Programming_Software_Tools/PYTHON/lab2/logger.py
+++ 4cem/Programming_Software_Tools/PYTHON/lab2/logger.py
@@ -4,24 +4,42 @@ import inspect
 class Logger(object):
     def __init__(self):
         self.log_array = []
+        self.logformat = ""
+        Logger.set_logger_format(self)
 
     def __str__(self):
         res_str = ""
         count = 1
         for call in self.log_array:
-            res_str += "{0})Name: {1}," \
-                       " *args: {2}," \
-                       " **kwargs: {3}," \
-                       " returned: {4} \n".format(count,
-                                                call[0],
-                                                call[1],
-                                                call[2],
-                                                call[3])
+            res_str += self.logformat.format(count,
+                                            call[0],
+                                            call[1],
+                                            call[2],
+                                            call[3])
             count += 1
         return str(res_str)
 
+    def set_logger_format(self, **kwargs):
+        s = "{0})"
+        if len(kwargs) == 0:
+            self.logformat = "{0})Name: {1}," \
+                   " *args: {2}," \
+                   " **kwargs: {3}," \
+                   " returned: {4} \n"
+        if "name" in kwargs:
+            s += " Name: {1} "
+        if "args" in kwargs:
+            s += " *args: {2} "
+        if "kwargs" in kwargs:
+            s += " **kwargs: {3} "
+        if "ret" in kwargs:
+            s += " returned: {4} "
+        s += "\n"
+        self.logformat = s
+
+
     def __getattribute__(self, name, *args, **kwargs):
-        if inspect.ismethod(object.__getattribute__(self, name)):
+        if inspect.ismethod(object.__getattribute__(self, name)) and name != "set_logger_format":
             def tmp(*args, **kwargs):
                 res = object.__getattribute__(self, name)(*args, **kwargs)
                 self.log_array.append((name, str(args), str(kwargs), res))
@@ -38,7 +56,7 @@ class Sample(Logger):
 
 def main():
     sam = Sample()
-
+    sam.set_logger_format(name=True, ret=True)
     sam.sample(3, 3)
     sam.sample(3, 1)
     sam.sample(3, 2)
