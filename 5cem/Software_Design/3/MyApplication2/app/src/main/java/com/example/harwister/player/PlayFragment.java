75171ed9fe1fbbba89c4d1e61f96c444bcb0d6a3
Nickolai Rovdo <nichogaus@gmail.com>
2016-11-03 18:13:15 +0300

    5.sd.

---
+++ 5cem/Software_Design/3/MyApplication2/app/src/main/java/com/example/harwister/player/PlayFragment.java
@@ -0,0 +1,128 @@
+package com.example.harwister.player;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PlayFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * create an instance of this fragment.
+ */
+public class PlayFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+
+    // TODO: Rename and change types of parameters
+
+    public List<Song> songs;
+    public Song current_song;
+    private OnFragmentInteractionListener mListener;
+    public MediaPlayer mediaPlayer;
+    public Activity activity;
+
+    public PlayFragment() {
+    }
+
+    // TODO: Rename and change types and number of parameters
+   /* public static PlayFragment newInstance(List<Song> kek) {
+        PlayFragment fragment = new PlayFragment();
+        fragment.songs = kek;
+        return fragment;
+    }*/
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v =inflater.inflate(R.layout.fragment_play, container, false);
+        final ImageView song_imageview = (ImageView) v.findViewById(R.id.song_imageview);
+
+        v.findViewById(R.id.next_song_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View ve) {
+//                song_imageview.setImageResource(R.drawable.ima1);
+               ((MusicActivity) getActivity()).playNextSong(song_imageview);
+
+            }
+        });
+
+//        v.findViewById(R.id.song_imageview)
+        // set image
+        return v;
+    }
+
+//    private void play() {
+//        mediaPlayer = MediaPlayer.create(activity, Uri.parse(current_song.filepath));
+//        mediaPlayer.start();
+//    }
+//
+//    private void playNextSong() {
+//      /*  int index = songs.indexOf(current_song);
+//        if (songs.size() == index - 1) current_song = songs.get(1);
+//        else current_song = songs.get(index + 1);*/
+//
+//    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ 5cem/Software_Design/3/MyApplication2/app/src/main/java/com/example/harwister/player/adapters/MusicAdapter.java
@@ -0,0 +1,136 @@
+
+package com.example.harwister.player.adapters;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.activeandroid.query.Select;
+import com.example.harwister.player.R;
+import com.example.harwister.player.Song;
+import java.util.Date;
+import java.util.List;
+
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder> {
+    private MediaPlayer mediaPlayer;
+    private List<Song> songs;
+    private Activity activity;
+
+    private int playingSongPosition = -1;
+
+
+    public MusicAdapter(List<Song> songs, Activity activity) {
+        this.activity = activity;
+        this.songs = songs;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new SongViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.song_cardview, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final MusicAdapter.ViewHolder holder, final int position) {
+        final SongViewHolder cholder = (SongViewHolder) holder;
+        cholder.name.setText(songs.get(position).name);
+        cholder.image.setImageResource(songs.get(position).imagepath);
+        cholder.root.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(activity, "playing", Toast.LENGTH_SHORT).show();
+                Song song_to_play = (Song) new Select().from(Song.class).where("name = ?", cholder.name.getText().toString()).execute().get(0);
+                if (mediaPlayer != null) mediaPlayer.stop();
+                mediaPlayer = MediaPlayer.create(activity, Uri.parse(song_to_play.filepath));
+                mediaPlayer.start();
+                Date date = new Date();
+                song_to_play.date = date;
+                song_to_play.save();
+                playingSongPosition = position;
+            }
+        });
+
+    }
+
+    public Dialog onCreateDialog(final String song_name) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        LayoutInflater inflater = activity.getLayoutInflater();
+        final View kek = inflater.inflate(R.layout.dialog_delete_song, null);
+        builder.setView(kek)
+                .setPositiveButton("DELETE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(activity, "deleting", Toast.LENGTH_SHORT).show();
+                        Song file_to_delete = (Song) new Select().from(Song.class).where("name = ?", song_name.toString()).execute().get(0);
+                        int index = songs.indexOf(file_to_delete);
+                        songs.remove(file_to_delete);
+                        notifyItemRemoved(index);
+                        file_to_delete.delete();
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+        builder.setTitle("Delete song?");
+        return builder.create();
+    }
+
+    @Override
+    public int getItemCount() {
+        return songs.size();
+    }
+
+    public class SongViewHolder extends ViewHolder {
+        TextView name;
+        ImageView image;
+        CardView root;
+
+        public SongViewHolder(View v) {
+            super(v);
+            name = (TextView) v.findViewById(R.id.textView4);
+            image = (ImageView) v.findViewById(R.id.imageView5);
+            root = (CardView) v.findViewById(R.id.song_card);
+
+            v.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    Dialog dialog = onCreateDialog(name.getText().toString());
+                    dialog.show();
+                    return true;
+                }
+            });
+        }
+    }
+
+    public void playNextSong(ImageView imageView) {
+        playingSongPosition = ++playingSongPosition % songs.size();
+        Toast.makeText(activity, "playing song number " + playingSongPosition, Toast.LENGTH_SHORT).show();
+        Song song_to_play = songs.get(playingSongPosition);
+        if (mediaPlayer != null) mediaPlayer.stop();
+        mediaPlayer = MediaPlayer.create(activity, Uri.parse(song_to_play.filepath));
+        imageView.setImageResource(song_to_play.imagepath);
+        mediaPlayer.start();
+        Date date = new Date();
+        song_to_play.date = date;
+        song_to_play.save();
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
