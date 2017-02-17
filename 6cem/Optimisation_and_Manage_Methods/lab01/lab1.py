4f4ea3e8fa20edadefc5b6781b2deedb1551a8a0
Nickolai Rovdo <nichogaus@gmail.com>
2017-02-17 09:52:12 +0300

    6.moiu.1 added tests

--- 6cem/Optimisation_and_Manage_Methods/lab01/lab1.py
+++ 6cem/Optimisation_and_Manage_Methods/lab01/lab1.py
@@ -1,31 +1,35 @@
-from config import *
-iter_count = 0
-J -= 1
-while True:
-    Theta = []
-    B = linalg.inv(A[:, J])
-    u = c[J].dot(B)
-    not_J = delete(arange(n), J)
-    deltas = list(map(lambda x: u.dot(A[:, x]) - c[x], list(not_J)))
-    min_delta_ind = not_J[argmin(deltas)]
-    if u.dot(A[:, min_delta_ind]) - c[min_delta_ind] >= 0:
-        print(list(map(lambda x: round(float(x), 3), list(x_0))), "- оптимальный план")
-        print("Максимальная прибыль : ", c.dot(x_0))
-        print("Количество итераций :", iter_count)
-        break
-    z = B.dot(A[:, min_delta_ind])
-    if max(z) <= 0:
-        print("Нет решений.")
-        break
-    for i, _z in zip(J, z):
-        if _z > 0:
-            Theta.append(x_0[i] / _z)
-        else:
-            Theta.append(inf)
-    s = argmin(Theta)
-    x_0[not_J] = 0
-    x_0[min_delta_ind] = Theta[s]
-    for index, _z in zip(J, z):
-        x_0[index] -= Theta[s] * _z
-    J[s] = min_delta_ind
-    iter_count += 1
+from numpy import *
+
+
+def simplex(m, n, A, b, c, x_0, J):
+    iter_count = 0
+    J -= 1
+    while True:
+        Theta = []
+        B = linalg.inv(A[:, J])
+        u = c[J].dot(B)
+        not_J = delete(arange(n), J)
+        deltas = list(map(lambda _not_j: u.dot(A[:, _not_j]) - c[_not_j], list(not_J)))
+        min_delta_ind = not_J[argmin(deltas)]
+        if min(deltas) >= 0:
+            print(list(map(lambda _x: round(float(_x), 3), list(x_0))), "- оптимальный план")
+            print("Максимальная прибыль : ", c.dot(x_0))
+            print("Количество итераций : ", iter_count)
+            return list(map(lambda _x: round(float(_x), 3), list(x_0))), c.dot(x_0), iter_count
+            break
+        z = B.dot(A[:, min_delta_ind])
+        if not all(_z <= 0 for _z in list(z)):
+            print("Нет решений.")
+            break
+        for i, _z in zip(J, z):
+            if _z > 0:
+                Theta.append(x_0[i] / _z)
+            else:
+                Theta.append(inf)
+        s = argmin(Theta)  # Theta arg equal Jb arg
+        x_0[not_J] = 0
+        x_0[min_delta_ind] = min(Theta)
+        for index, _z in zip(J, z):
+            x_0[index] -= min(Theta) * _z
+        J[s] = min_delta_ind
+        iter_count += 1
