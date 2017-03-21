9865bdd8435857169fbb5b570ee61d6a595b622f
Nickolai Rovdo <nichogaus@gmail.com>
2017-03-21 14:27:44 +0300

    6.moui.5 corrections

--- 6cem/Optimisation_and_Manage_Methods/lab06/lab06_27.py
+++ 6cem/Optimisation_and_Manage_Methods/lab06/lab06_27.py
@@ -7,26 +7,26 @@ def function_g_x(x, B, c, alpha):  # g(x)
     return 0.5 * x.dot(B.T).dot(B).dot(x) + c.dot(x) + alpha
 
 
-def function_f_x(x, B, c):
-    return 0.5 * B.T.dot(B).dot(x) + c
+def function_df_dx(x, B, c):
+    return B.T.dot(B).dot(x) + c
 
 
-def analyse(B0, c0, B, c, alpha, x):
+def convex(B0, c0, B, c, alpha, x):
     J0 = [index for index in xrange(len(x)) if x[index] == 0.0]
     g = array([function_g_x(x, B_i, c_i, alpha_i) for B_i, c_i, alpha_i in zip(B, c, alpha)])
     print list(x), "- начальный план" if (g <= 0).all() else exit()  # проверим исходный план
     print "Значение целевой функции по начальному плану f(x) =", function_g_x(x, B0, c0, 0.0)
+    print "Проверка оптимальности начального плана..."
     I0 = array([index for index in xrange(len(g)) if g[index] == 0.0])
-    df = function_f_x(x, B0, c0)
-    dg_I0 = array([function_f_x(x, B[i], c[i]) for i in I0])
+    df = function_df_dx(x, B0, c0)
+    dg_I0 = array([function_df_dx(x, B[i], c[i]) for i in I0])
     # границы
     d_m = zeros(len(x))  # dm
     d_m[delete(arange(len(x)), J0)] = -1
-    print "Проверка оптимальности начального плана..."
     l = opt.linprog(df, A_ub=dg_I0, b_ub=zeros(len(dg_I0)), bounds=zip(d_m, ones(len(x))))
     f = l.fun
     l = l.x  # l0
-    print "План не оптимален." if f < 0 else exit()
+    print "План не оптимален. Значение dg/dx", f if f < 0 else exit()
     a = 1  # подберём число А для неравенства
     delta_x = zeros(len(x)) - x
     k = df.dot(delta_x)
@@ -37,12 +37,10 @@ def analyse(B0, c0, B, c, alpha, x):
         a = a0 + 1
     t = 2.0  # подберём число t для неравенства
     while True:
-        x_t = x + t * (l + a * delta_x)
-        _g = array([function_g_x(x_t, B_i, c_i, alpha_i) for B_i, c_i, alpha_i in zip(B, c, alpha)])
-        if (_g <= 0.0).all() and (x_t >= 0.0).all():
+        x_t = x + t * (l + a * delta_x)  # новый план
+        if (array([function_g_x(x_t, B_i, c_i, alpha_i) for B_i, c_i, alpha_i in zip(B, c, alpha)]) <= 0.0).all() and (x_t >= 0.0).all():
             break
         t /= 2.0
-    x_t = x + t * (l + a * delta_x)
     print "Новый план  x_t =", list(x_t)
     print "Новое значение целевой функции f(x_t) =", function_g_x(x_t, B0, c0, 0.0)  # less then f(x*)
-    print "Улучшение плана: ", 100 - function_g_x(x_t, B0, c0, 0.0) / function_g_x(x, B0, c0, 0.0) * 100
+    print "Улучшение плана: ", round(100 - function_g_x(x_t, B0, c0, 0.0) / function_g_x(x, B0, c0, 0.0) * 100, 2), "%"
