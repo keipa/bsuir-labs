c3816b81aaf1e81d1fc5f4c1322198a1d94701c9
Nickolai Rovdo <nichogaus@gmail.com>
2017-05-09 13:45:03 +0300

    6.mtran.exam

--- 6cem/Translation_Methods/lab04/front/PascalInterpreter/PascalInterpreter/Program.cs
+++ 6cem/Translation_Methods/lab04/front/PascalInterpreter/PascalInterpreter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace PascalInterpreter
     {
         static void Main(string[] args)
         {
+
             var command = "start.bat";
             var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
             processInfo.CreateNoWindow = true;
@@ -22,27 +24,21 @@ namespace PascalInterpreter
             string output = process.StandardOutput.ReadToEnd();
             string error = process.StandardError.ReadToEnd();
             var exitCode = process.ExitCode;
-            Console.Clear();
 
             Console.WriteLine(string.IsNullOrEmpty(error) ? string.Empty : error.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Last());
-            //Console.WriteLine("On line: " + new Random().Next(20, 50) + " on Symbol: " + new Random().Next(10, 20));
-            int n = 12;
-            string[] lines = output
-                .Split(Environment.NewLine.ToCharArray())
-                .Skip(n)
-                .ToArray();
-            string output2 = string.Join(Environment.NewLine, lines).Replace("\r\n\r\n", "\r\n");
-            output2 = output2.Replace("Error: C:\\FPC\\3.0.2\\bin\\i386-Win32\\ppc386.exe returned an error exitcode",
-                String.Empty);
-            output2 = output2.Replace("Compiling Project2.~dpr",String.Empty);
-            
-            output2 = output2.Replace("Target OS: Win32 for i386", String.Empty);
-            Console.WriteLine((String.IsNullOrEmpty(output2) ? "(none)" : output2));
-
+          
+         
             Console.WriteLine("ExitCode: " + exitCode.ToString(), "ExecuteCommand");
             process.Close();
-            
-            
+
+            string text = File.ReadAllText("C:/Users/keipa/Desktop/print/hello.ppc");
+            text = text.Replace("printf", "writeln");
+            File.WriteAllText("C:\\Users\\keipa\\Desktop\\print\\hello.ppc", text);
+
+
+
+
+
             Console.ReadKey();
         }
     }
