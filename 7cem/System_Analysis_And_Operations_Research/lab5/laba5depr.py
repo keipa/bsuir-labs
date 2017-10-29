79212ac35e1ba3ac2702f6832bdb582fdafc69b1
keipa <nichogaus@gmail.com>
2017-10-29 16:56:05 +0300

    7.saio.6

--- 7cem/System_Analysis_And_Operations_Research/lab5/laba5depr.py
+++ 7cem/System_Analysis_And_Operations_Research/lab5/laba5depr.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python2.7
 from numpy import *
 
 
@@ -9,49 +8,48 @@ def swap(a, b):
     return a, b
 
 
-def make_basis_graf(n, S):
-    g = dict()
-    for i in range(n):
-            g[i] = []
+def GetSpectialEdgeIndex(graph ,source, destination):
+    for edge in graph:
+        if source == edge.source and destination == edge.destination:
+            return graph.index(edge)
 
+def GetBasis(n, graph):
+    basis = {i: [] for i in range(n)}
     for i in range(n):
-        for j in range(len(S)):
-            if S[j][4] == 1:
-                if i == S[j][0]:
-                    g[i].append(S[j][1])
-                    g[S[j][1]].append(i)
-
-    return g
+        for j in range(len(graph.edges)):
+            if graph.edges[j].isBasis:
+                if i == graph.edges[j].source:
+                    basis[i].append(graph.edges[j].destination)
+                    basis[graph.edges[j].destination].append(i)
+    return basis
 
 
 def dfs_u(v, g, S, u, used):
     used[v] = True
     for to in g[v]:
-        if used[to] == False:
-            for elem in S:
-                if v == elem[0] and to == elem[1]:
-                    u[to] = u[v] - elem[2]
-                elif v == elem[1] and to == elem[0]:
-                    u[to] = u[v] + elem[2]
+        if not used[to]:
+            for elem in S.edges:
+                if v == elem.source and to == elem.destination:
+                    u[to] = u[v] - elem.cost
+                elif v == elem.destination and to == elem.source:
+                    u[to] = u[v] + elem.cost
             dfs_u(to, g, S, u, used)
 
 
-def solve_potensial(n, g, S):
-    used = [False for i in range(n)]
-    u = [inf for i in range(n)]
+def GetPotentials(n, g, S):
+    used = [False for _ in range(n)]
+    u = [inf for _ in range(n)]
     u[0] = 0
     dfs_u(0, g, S, u, used)
     return u
 
 
-def solve_marks(S, u):
+def GetDeltasEdgesAndValues(S, u):
     delta = []
-    for elem in S:
-        if elem[4] == 0:
-            delta.append(((elem[0], elem[1]), u[elem[0]] - u[elem[1]] - elem[2]))
-
+    for edge in S.edges:
+        if not edge.isBasis:
+            delta.append(((edge.source, edge.destination), u[edge.source] - u[edge.destination] - edge.cost))
     return delta
-
 
 
 def dfs(v, color, p, g):
@@ -59,9 +57,8 @@ def dfs(v, color, p, g):
     for to in g[v]:
         if color[to] == 0:
             p[to] = v
-            if (dfs(to, color, p, g)):
+            if dfs(to, color, p, g):
                 return True
-
         elif (color[to] == 1) and (p[v] != to):
             global cycle_st
             global cycle_end
@@ -79,11 +76,11 @@ def cycle(uu, n, g):
     for i in range(n):
         color.append(0)
         p.append(-1)
-
+    _cycle = []
     if dfs(uu, color, p, g):
-        _cycle = []
+
         v = cycle_end
-        while(v != cycle_st):
+        while v != cycle_st:
             _cycle.append(v)
             v = p[v]
 
@@ -92,92 +89,99 @@ def cycle(uu, n, g):
     return _cycle
 
 
+def GetMaxDelta(delta):
+    values = [i[1] for i in delta]
+    return delta[argmax(values)][0], max(values), argmax(values)
+
+
 def matrics_network_task(n, S):
+    net = PotentialGragh(S)
     while True:
-        g1 = make_basis_graf(n, S)
-        u = solve_potensial(n, g1, S)
-        delta = solve_marks(S, u)
-        temp_list = [elem[1] for elem in delta]
-        max_delta = max(temp_list)
-        if max_delta <= 0:
+        bas = GetBasis(n, net)
+        pot = GetPotentials(n, bas, net)
+        delta = GetDeltasEdgesAndValues(net, pot)
+        max_delta_edge_i0j0, max_delta_value, max_delta_ind = GetMaxDelta(delta)
+        if max_delta_value <= 0:
             sum = 0
-            for elem in S:
-                sum += elem[2]*elem[3]
+            for elem in net.edges:
+                sum += elem.x * elem.cost # x * cost
 
             print(sum)
-            return S
+            [print(str(edge.source+1)+":"+str(edge.destination+1)+" "+str(edge.x)) for edge in net.edges]
+            return net.edges
 
-        ind = temp_list.index(max_delta)
-        curve_0 = delta[ind][0]
-        for i in range(len(S)):
-            if S[i][0] == curve_0[0] and S[i][1] == curve_0[1]:
-                S[i][4] = 1
+        for edge in net.edges:
+            if edge.source == max_delta_edge_i0j0[0] and edge.destination == max_delta_edge_i0j0[1]:
+                edge.isBasis = True
 
-        g2 = make_basis_graf(n, S)
-        U = cycle(curve_0[0], n, g2)
+        g2 = GetBasis(n, net)
+        U = cycle(max_delta_edge_i0j0[0], n, g2)
         U.reverse()
         U.append(U[0])
-        U_plus = []
-        U_minus= []
-        for i in range(len(U) - 1):
-            for elem in S:
-                if U[i] == elem[0] and U[i+1] == elem[1]:
-                    U_plus.append((U[i], U[i+1]))
-                elif U[i] == elem[1] and U[i+1] == elem[0]:
-                    U_minus.append((U[i+1], U[i]))
+        U_basis_plus = []
+        U__basis_minus = []
+        for edge in range(len(U) - 1):
+            for elem in net.edges:
+                if U[edge] == elem.source and U[edge + 1] == elem.destination:
+                    U_basis_plus.append((U[edge], U[edge + 1]))
+                elif U[edge] == elem.destination and U[edge + 1] == elem.source:
+                    U__basis_minus.append((U[edge + 1], U[edge]))
 
-        if curve_0 not in U_plus:
-            U_minus, U_plus = swap(U_minus, U_plus)
+        if max_delta_edge_i0j0 not in U_basis_plus:
+            U__basis_minus, U_basis_plus = swap(U__basis_minus, U_basis_plus)
 
         tetta = []
-        for elem in S:
-            tupl = (elem[0], elem[1])
-            if tupl in U_minus:
-                tetta.append((tupl, elem[3]))
+        for elem in net.edges:
+            tupl = (elem.source, elem.destination)
+            if tupl in U__basis_minus:
+                tetta.append((tupl, elem.x))
 
         tetta0 = min([tetta[i][1] for i in range(len(tetta))])
+
         for elem in tetta:
             if elem[1] == tetta0:
                 curve_star = elem[0]
                 break
 
-        for curve in U_plus:
-            for i in range(len(S)):
-                if curve[0] == S[i][0] and curve[1] == S[i][1]:
-                    S[i][3] += tetta0
-                    break
+        for curve in U_basis_plus:
+            net.edges[GetSpectialEdgeIndex(net.edges, curve[0], curve[1])].x += tetta0
 
-        for curve in U_minus:
-            for i in range(len(S)):
-                if curve[0] == S[i][0] and curve[1] == S[i][1]:
-                    S[i][3] -= tetta0
-                    break
+        for curve in U__basis_minus:
+            net.edges[GetSpectialEdgeIndex(net.edges, curve[0], curve[1])].x -= tetta0
 
-        for i in range(len(S)):
-            if curve_star[0] == S[i][0] and curve_star[1] == S[i][1]:
-                S[i][4] = 0
-                break
+        net.edges[GetSpectialEdgeIndex(net.edges, curve_star[0], curve_star[1])].isBasis = False
 
 
-if __name__ == "__main__":
-    cycle_st = -1
-    cycle_end = -10
-    S = [
-        [0, 1, 7, 2, 1],
-        [0, 2, 6, 3, 1],
-        [2, 3, 6, 4, 1],
-        [2, 4, 5, 4, 1],
-        [5, 6, 4, 2, 1],
-        [6, 4, 7, 5, 1],
-        [1, 2, 4, 0, 0],
-        [1, 5, 3, 0, 0],
-        [3, 5, 1, 0, 0],
-        [4, 3, 4, 0, 0],
-        [4, 5, -1, 0, 0],# from-1 /to-1/ red cost / black X/ 0- 1 - not basis and basis
-        [0, 4, 3, 0, 0],
-        [6, 0, 2, 0, 0]
-    ]
-    n = 7
-    res = matrics_network_task(n, S)
-    print(res)
+
+class Edge:
+    def __init__(self, rawEdge):
+        self.source = rawEdge[0]
+        self.destination = rawEdge[1]
+        self.cost = rawEdge[2]
+        self.x = rawEdge[3]
+        self.isBasis = True if rawEdge[4] == 1 else False
+
+
+class PotentialGragh:
+    def __init__(self, rawdata):
+        self.raw = rawdata
+        self.edges = self.GetEdges()
+
+    def GetEdges(self):
+        edgesList = list()
+        for i in self.raw:
+            edge = Edge(i)
+            edgesList.append(edge)
+        return edgesList
+
+
+def GetInput(path="05"):
+    raw =  []
+    with open(path) as f:
+        for line in f.readlines():
+            raw.append([int(number) for number in line.split(" ")])
+    return raw
+
+n = 7
+res = matrics_network_task(7, GetInput('05'))
 
---
+++ 7cem/System_Analysis_And_Operations_Research/lab6/diskstra.py
@@ -0,0 +1,75 @@
+from collections import defaultdict, deque
+
+
+class Graph(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(list)
+        self.distances = {}
+
+    def add_node(self, value):
+        self.nodes.add(value)
+
+    def add_edge(self, from_node, to_node, distance):
+        self.edges[from_node].append(to_node)
+        self.edges[to_node].append(from_node)
+        self.distances[(from_node, to_node)] = distance
+
+
+def dijkstra(graph, initial):
+    visited = {initial: 0}
+    path = {}
+
+    nodes = set(graph.nodes)
+
+    while nodes:
+        min_node = None
+        for node in nodes:
+            if node in visited:
+                if min_node is None:
+                    min_node = node
+                elif visited[node] < visited[min_node]:
+                    min_node = node
+        if min_node is None:
+            break
+
+        nodes.remove(min_node)
+        current_weight = visited[min_node]
+
+        for edge in graph.edges[min_node]:
+            try:
+                weight = current_weight + graph.distances[(min_node, edge)]
+            except:
+                continue
+            if edge not in visited or weight < visited[edge]:
+                visited[edge] = weight
+                path[edge] = min_node
+
+    return visited, path
+
+
+def shortest_path(graph, origin, destination):
+    visited, paths = dijkstra(graph, origin)
+    full_path = deque()
+    _destination = paths[destination]
+
+    while _destination != origin:
+        full_path.appendleft(_destination)
+        _destination = paths[_destination]
+
+    full_path.appendleft(origin)
+    full_path.append(destination)
+
+    return visited[destination], list(full_path)
+
+
+graph = Graph()
+for node in range(1, 7 + 1):
+    graph.add_node(node)
+
+with open('01') as f:
+    for line in f.readlines():
+        edge = [int(number) for number in line.split(" ")]
+        graph.add_edge(edge[0], edge[1], edge[2])
+
+print(shortest_path(graph, 1, 4))
