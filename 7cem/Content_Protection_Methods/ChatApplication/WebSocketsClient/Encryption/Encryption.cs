d94c4b95c08ee47af25b99345bf172011b836123
keipa <nichogaus@gmail.com>
2017-12-02 15:53:31 +0300

    7.mzi.aes-rsa

---
+++ 7cem/Content_Protection_Methods/ChatApplication/WebSocketsClient/Encryption/Encryption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketsClient.Encryption
+{
+    static class Encryption
+    {
+        public static string EncAPI(string message)
+        {
+            return $"http://localhost:58850/api/des/{message}/encrypt";
+        }
+    }
+}
