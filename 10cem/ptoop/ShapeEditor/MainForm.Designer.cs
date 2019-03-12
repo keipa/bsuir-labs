80cac5d90344f4bfafbf78b3814481cc5f95086a
Nickolai Rovdo <nichogaus@gmail.com>
2019-03-12 23:53:57 +0300

    priviting

--- 10cem/ptoop/ShapeEditor/MainForm.Designer.cs
+++ 10cem/ptoop/ShapeEditor/MainForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class MainForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.shapePictureBox = new System.Windows.Forms.PictureBox();
@@ -44,9 +33,9 @@
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
+            //
             // shapePictureBox
-            // 
+            //
             this.shapePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.shapePictureBox.Location = new System.Drawing.Point(12, 25);
             this.shapePictureBox.Name = "shapePictureBox";
@@ -54,9 +43,9 @@
             this.shapePictureBox.TabIndex = 0;
             this.shapePictureBox.TabStop = false;
             this.shapePictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.shapePictureBox_Paint);
-            // 
+            //
             // interpretTextArea
-            // 
+            //
             this.interpretTextArea.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.interpretTextArea.Location = new System.Drawing.Point(0, 0);
             this.interpretTextArea.Multiline = true;
@@ -64,9 +53,9 @@
             this.interpretTextArea.Size = new System.Drawing.Size(332, 373);
             this.interpretTextArea.TabIndex = 1;
             this.interpretTextArea.KeyDown += new System.Windows.Forms.KeyEventHandler(this.interpretTextArea_KeyDown);
-            // 
+            //
             // interpretButton
-            // 
+            //
             this.interpretButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.interpretButton.Location = new System.Drawing.Point(0, 379);
             this.interpretButton.Name = "interpretButton";
@@ -75,9 +64,9 @@
             this.interpretButton.Text = "Apply";
             this.interpretButton.UseVisualStyleBackColor = true;
             this.interpretButton.Click += new System.EventHandler(this.interpretButton_Click);
-            // 
+            //
             // errorLabel
-            // 
+            //
             this.errorLabel.BackColor = System.Drawing.SystemColors.ControlLight;
             this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.errorLabel.ForeColor = System.Drawing.Color.Firebrick;
@@ -86,9 +75,9 @@
             this.errorLabel.Size = new System.Drawing.Size(914, 42);
             this.errorLabel.TabIndex = 3;
             this.errorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
+            //
             // tabControl1
-            // 
+            //
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Location = new System.Drawing.Point(580, 25);
@@ -96,9 +85,9 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(346, 464);
             this.tabControl1.TabIndex = 4;
-            // 
+            //
             // tabPage1
-            // 
+            //
             this.tabPage1.Controls.Add(this.interpretTextArea);
             this.tabPage1.Controls.Add(this.interpretButton);
             this.tabPage1.Location = new System.Drawing.Point(4, 29);
@@ -107,9 +96,9 @@
             this.tabPage1.Size = new System.Drawing.Size(338, 431);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Interpret";
-            // 
+            //
             // tabPage2
-            // 
+            //
             this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
             this.tabPage2.Controls.Add(this.saveToFileButton);
             this.tabPage2.Controls.Add(this.openFIleButton);
@@ -119,9 +108,9 @@
             this.tabPage2.Size = new System.Drawing.Size(338, 431);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Save\\Load";
-            // 
+            //
             // saveToFileButton
-            // 
+            //
             this.saveToFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.saveToFileButton.Location = new System.Drawing.Point(3, 379);
             this.saveToFileButton.Name = "saveToFileButton";
@@ -130,9 +119,9 @@
             this.saveToFileButton.Text = "Save to file";
             this.saveToFileButton.UseVisualStyleBackColor = true;
             this.saveToFileButton.Click += new System.EventHandler(this.saveToFileButton_Click);
-            // 
+            //
             // openFIleButton
-            // 
+            //
             this.openFIleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.openFIleButton.Location = new System.Drawing.Point(3, 327);
             this.openFIleButton.Name = "openFIleButton";
@@ -141,9 +130,9 @@
             this.openFIleButton.Text = "Open file";
             this.openFIleButton.UseVisualStyleBackColor = true;
             this.openFIleButton.Click += new System.EventHandler(this.openFileButton_Click);
-            // 
+            //
             // MainForm
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(938, 543);
