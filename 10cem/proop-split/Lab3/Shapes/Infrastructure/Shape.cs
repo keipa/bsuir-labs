94bdf8c96e9bad5b9c3e34baacf598a1c2e24be3
Nickolai Rovdo <nichogaus@gmail.com>
2019-03-17 19:15:39 +0300

    adding basic lab3

---
+++ 10cem/proop-split/Lab3/Shapes/Infrastructure/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Infrastructure
+{
+    public class Shape
+    {
+        public Type Type { get; set; }
+        public int X { get; set; }
+        public int Y { get; set;  }
+    }
+}
