53a97e08c55e016e545452c2724009d82af6a6d9
Nickolai Rovdo <nichogaus@gmail.com>
2019-03-17 19:15:39 +0300

    adding basic lab3

---
+++ 10cem/proop-split/Lab3/Lab3/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Shapes;
+using Shapes.Infrastructure;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        private static Form _mainForm;
+        private static Label _introLabel;
+        private static Form _addShapeForm;
+        private static int X;
+        private static int Y;
+        private static ActualDrawer drawer;
+        public static List<Shape> _shapes = new List<Shape>();
+
+        public static readonly IEnumerable<Type> shapeTypes = Assembly.Load(typeof(Aggregation).Assembly.FullName)
+            .GetTypes()
+            .Where(type => type.GetInterfaces().Contains(typeof(IShape)) &&
+                           type != typeof(Aggregation));
+
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Application.Run(CreateMainForm());
+        }
+
+        private static Form CreateMainForm()
+        {
+            _mainForm = new Form
+            {
+                Size = new Size(400, 400)
+            };
+            _introLabel = new Label
+            {
+                Text = "Please, click somewhere on form",
+                Location = new Point(_mainForm.Size.Width / 2, _mainForm.Size.Height / 2),
+                Size = new Size(100, 100)
+            };
+            _mainForm.Controls.Add(_introLabel);
+            
+            _mainForm.MouseClick += Form_MouseClick;
+            _introLabel.MouseClick += Form_MouseClick;
+            return _mainForm;
+        }
+
+        private static void Form_MouseClick(object sender, MouseEventArgs e)
+        {
+            _introLabel.Hide();
+            switch (e.Button) {
+
+                case MouseButtons.Left:
+                    AddShapeDialog(e);
+                    break;
+
+                case MouseButtons.Right:
+                    var serializeForm = new SerializeForm();
+                    serializeForm.Show();
+                    break;
+            }
+        }
+
+        private static void AddShapeDialog(MouseEventArgs e)
+        {
+            _addShapeForm = new Form
+            {
+                Size = new Size(200, 200)
+            };
+            var label = new Label
+            {
+                Text = $"Coordinates for shape x :{e.X} y: {e.Y}"
+            };
+            X = e.X;
+            Y = e.Y;
+
+            var shapesListView = new ListView
+            {
+                Size = new Size(200, 100),
+                Location = new Point(0, 50)
+            };
+
+            shapesListView.ItemActivate += ShapesListViewOnItemActivate;
+
+            foreach (var shape in shapeTypes.Select(t => t.Name)) shapesListView.Items.Add(shape);
+            _addShapeForm.Controls.Add(label);
+            _addShapeForm.Controls.Add(shapesListView);
+            _addShapeForm.Show();         
+        }
+
+
+        private static void ShapesListViewOnItemActivate(object sender, EventArgs e)
+        {
+            var shapeTypeToDraw = shapeTypes.FirstOrDefault(sh => sh.Name == ((ListView) sender).SelectedItems[0].Text);
+
+            AddShapeToForm(shapeTypeToDraw, X, Y);
+
+            _addShapeForm.Hide();
+        }
+        
+        public static void AddShapeToForm(Type type, int x, int y)
+        {
+            _shapes.Add(new Shape
+            {
+                Type = type,
+                X = x,
+                Y = y
+            });
+
+            var context = new DrawingContext();
+            var drawer = new ActualDrawer(x, y, context);
+            var shape = (IShape)Activator.CreateInstance(type, drawer);
+            _mainForm.Paint += (o, ee) => { shape.Draw(ee.Graphics); };
+
+            _mainForm.Invalidate(); // update form magic 
+        }
+
+        public static void ClearForm()
+        {
+            _mainForm.Paint += (o, ee) => { ee.Graphics.Clear(Color.WhiteSmoke); };
+            _mainForm.Invalidate();
+            _introLabel.Show();
+        }
+
+        public static void RestoreShapes(IEnumerable<Shape> restoredShapes)
+        {
+            foreach(var restoredShape in restoredShapes)
+            {
+                AddShapeToForm(restoredShape.Type, restoredShape.X, restoredShape.Y);
+            }
+        }
+    }
+}
---
+++ 10cem/proop-split/Lab3/Lab3/SerializeForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Lab1
+{
+    partial class SerializeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listOfShapesView = new System.Windows.Forms.ListBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listOfShapesView
+            // 
+            this.listOfShapesView.FormattingEnabled = true;
+            this.listOfShapesView.Location = new System.Drawing.Point(0, 0);
+            this.listOfShapesView.Name = "listOfShapesView";
+            this.listOfShapesView.Size = new System.Drawing.Size(265, 355);
+            this.listOfShapesView.TabIndex = 0;
+            this.listOfShapesView.SelectedIndexChanged += new System.EventHandler(this.listOfShapesView_SelectedIndexChanged);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(12, 361);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 1;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(178, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Open";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(93, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // SerializeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.listOfShapesView);
+            this.Name = "SerializeForm";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listOfShapesView;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
