7d493c92162b004717f54aa4a94ed102ca8bd8df
keipa <nichogaus@gmail.com>
2019-10-27 16:02:12 +0300

    ml

--- 11cem/ml/pd.py
+++ 11cem/ml/pd.py
@@ -12,5 +12,17 @@ print(data.sort_values(by="Profit"))
 
 # data.plot.bar()
 
-data.sort_values(by="Popularity").plot.line(x='Popularity', y='Profit', c='DarkBlue')
+data.sort_values(by="Popularity").plot.scatter(x='Popularity', y='Profit', c='DarkBlue')
+plt.grid()
+plt.axis('equal')
+
+x = range(0,25,1)
+k = 1.5
+b = -7
+y = [i*k+b for i in x]
+plt.plot(x, y)
 plt.show()
+
+
+
+
