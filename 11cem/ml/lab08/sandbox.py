5f23f8d3188549bc5294a7f192a872db43f6a0e5
Nickolai Rovdo <nichogaus@gmail.com>
2019-11-11 21:51:12 +0300

    ml 8.5

--- 11cem/ml/lab08/sandbox.py
+++ 11cem/ml/lab08/sandbox.py
@@ -11,3 +11,7 @@ with sns.axes_style('white'):
     sns.jointplot(X[:,0],X[:,1]).plot_joint(sns.kdeplot)
 
 plt.show()
+
+from sklearn import metrics
+#F1 = 2 * (precision * recall) / (precision + recall)
+print(metrics.f1_score([1,1,1], [1,1,1]))
